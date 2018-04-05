a2d6052a602f38a202f0b7397ec8c14be3f9b4c4
merey90 <merey.90@gmail.com>
2018-04-05 20:48:50 +0200

    my first submission with score 26.3413

--- dengAI.R
+++ dengAI.R
@@ -1,72 +1,94 @@
 library(data.table)
-#library(missForest)
+library(missForest)
 library(randomForest)
 library(dplyr)
 library(ggplot2)
-#library(doParallel)
+library(caret)
+library(doParallel)
 
 df.features <- fread("dengue_features_train.csv", na.strings = c("","NA","?", " ", NA, "NaN", "Inf", "<NA>"))
 df.labels <- fread("dengue_labels_train.csv", na.strings = c("","NA","?", " ", NA, "NaN", "Inf", "<NA>"))
 df.test.features <- fread("dengue_features_test.csv", na.strings = c("","NA","?", " ", NA, "NaN", "Inf", "<NA>"))
 df.submit <- fread("submission_format.csv", na.strings = c("","NA","?", " ", NA, "NaN", "Inf", "<NA>"))
-# 273.15
-rm(df.features, df.labels, df.test.features)
 
 df.data <- inner_join(df.features,df.labels)
 df.test <- inner_join(df.test.features,df.submit)
-#MAE()
+
+rm(df.features, df.labels, df.test.features)
+
+#ggplot(df.data, aes(x = precipitation_amt_mm, y = reanalysis_sat_precip_amt_mm)) + geom_smooth()
+
+preprocessData <- function(df){
+  df$week_start_date <- NULL
+  df$precipitation_amt_mm <- NULL
+  df$year <- NULL
+  
+  df$reanalysis_air_temp_k <- df$reanalysis_air_temp_k - 273.15
+  df$reanalysis_avg_temp_k <- df$reanalysis_avg_temp_k - 273.15
+  df$reanalysis_dew_point_temp_k <- df$reanalysis_dew_point_temp_k - 273.15
+  df$reanalysis_max_air_temp_k <- df$reanalysis_max_air_temp_k - 273.15
+  df$reanalysis_min_air_temp_k <- df$reanalysis_min_air_temp_k - 273.15
+  
+  df$city <- as.factor(as.character(df$city))
+  df$weekofyear <- as.factor(as.character(df$weekofyear))
+  
+  return(df)
+}
+
+sumVegetations <- function(df){
+  df$ndvi <- df$ndvi_ne + df$ndvi_nw + df$ndvi_se + df$ndvi_sw
+  df$ndvi_ne <- NULL
+  df$ndvi_nw <- NULL 
+  df$ndvi_se <- NULL
+  df$ndvi_sw <- NULL
+  return(df)
+}
+
+
+df.data <- preprocessData(df.data)
+df.test <- preprocessData(df.test)
+
 sapply(df.data, function(x){sum(is.na(x))})
-sapply(df.test, function(x){sum(is.na(x))})
 
 hist(df.data$total_cases)
 
-ggplot(df.data, aes(x = precipitation_amt_mm, y = reanalysis_sat_precip_amt_mm)) + geom_smooth()
-summary(train)
-summary(test)
+summary(df.data)
+summary(df.test)
 
-df.data$week_start_date <- NULL
-df.test$week_start_date <- NULL
-
-train$city <- as.factor(train$city)
-test$city <- as.factor(test$city)
-df <- rbind(train,test)
-str(train)
+df <- rbind(df.data,df.test)
 registerDoParallel(cores = 4)
 m <- missForest(df, ntree = 100, parallelize = "forests")
-m_train <- missForest(train, ntree = 1, parallelize = "forests")
-m_test <- missForest(train, ntree = 1, parallelize = "forests")
-
 df <- m$ximp
+rm(df, m)
+df.data <- df[c(1:1456),]
+df.test <- df[-c(1:1456),]
 
-train <- df[c(1:1456),]
-test <- df[-c(1:1456),]
+sapply(df.data, function(x){sum(is.na(x))})
 
-table(test$total_cases)
-
+df.data <- sumVegetations(df.data)
+df.test <- sumVegetations(df.test)
+str(df.data)
 #################
-
-library(caret)
-index <- createDataPartition(train$total_cases,p=0.8,list = FALSE)
-df_train <- train[index,]
-df_test <- train[-index,]
+index <- createDataPartition(df.data$total_cases,p=0.75,list = FALSE)
+df.data.train <- df.data[index,]
+df.data.test <- df.data[-index,]
 
 #table(train$yn)
 #table(test$yn)
 
-rf <- randomForest(total_cases~.,df_train,ntree=300,do.trace=TRUE)
-pred <- predict(rf,df_test)
+rf.train <- randomForest(total_cases~.,df.data.train,ntree=300,do.trace=TRUE)
+pred <- predict(rf,df.data.test)
 
 varImpPlot(rf)
-
-library(ROSE)
-roc.curve(df_test$total_cases,pred)
-
+df.data.test$pred <- pred
+ggplot(df.data.test, aes(x = total_cases, y = pred)) + geom_bar()
+roc.curve(df.data.test$total_cases,pred)
+MAE(df.data.test$total_cases,pred)
 ####################################
+rf <- randomForest(total_cases~.,df.data,ntree=300,do.trace=TRUE)
+pred.final <- predict(rf,df.test)
 
+df.submit$total_cases <- round(pred.final)
 
-pred_test <- predict(rf,test)
-
-df0 <- df4
-df0$total_cases <- round(pred_test)
-
-write.csv(df0,"~/resources/rstudio/fin.csv",row.names = FALSE)
+write.csv(df.submit,"fin.csv",row.names = FALSE)
+#26.3413
